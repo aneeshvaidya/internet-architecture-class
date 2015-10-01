b3646a1c0a806bc60c943bbd41d1b97fd99b27ca
rahim77 <krutchm@gmail.com>
2015-10-01 12:53:44 -0700

    made test_update trying to go through it

--- project1/simulator/dv_router.py
+++ project1/simulator/dv_router.py
@@ -4,6 +4,7 @@ Your awesome Distance Vector router for CS 168
 
 import sim.api as api
 import sim.basics as basics
+import pdb
 
 
 # We define infinity as a distance of 16.
@@ -12,8 +13,8 @@ INFINITY = 16
 
 class DVRouter (basics.DVRouterBase):
   NO_LOG = True # Set to True on an instance to disable its logging
-  POISON_MODE = True # Can override POISON_MODE here
-  DEFAULT_TIMER_INTERVAL = 5 # Can override this yourself for testing
+  POISON_MODE = False # Can override POISON_MODE here
+  #DEFAULT_TIMER_INTERVAL = 5 # Can override this yourself for testing
   
 
   def __init__ (self):
@@ -52,27 +53,17 @@ class DVRouter (basics.DVRouterBase):
 
     The port number used by the link is passed in.
     """
-    del self.neighbors[port]    
-    if port in self.DV.keys(): # router fall off
+    self.neighbors[port] = INFINITY  
+    if port in self.DV.keys():  # router fall off
         del self.DV[port]
-    else:                       # host fall off
-        for dest,v in self.me.iteritems():
-            l,p,t = v
-            if p == port:
-                del self.me[dest]
-                return
+    for dest,v in self.me.iteritems():
+        l,p,t = v
+        if p == port:
+            self.me[dest] = (INFINITY, p, 0)
+            if self.POISON_MODE:     # poisoning route
+                send_all_update(dest)
+            
 
-        
-        
-        
-    # for k,v in :
-        # if port in  and v[0] == 1:
-            # del self.host_ports[port]
-            # del self.vectors[k]
-            #send update
-        # elif port in v:
-            # del self.host_ports[port]
-            #recalculation and update
 
   def handle_rx (self, packet, port):
     """
@@ -83,15 +74,19 @@ class DVRouter (basics.DVRouterBase):
 
     You definitely want to fill this in.
     """
+    #pdb.set_trace()
     #self.log("RX %s on %s (%s)", packet, port, api.current_time())
     if isinstance(packet, basics.RoutePacket):
+        #pdb.set_trace()
         # if packet.dst in self.vector.keys():
             # if len(packet.trace) < 16 and packet.latency < self.vectors[packet.dst]:
                 # self.vectors[packet.dst] = [len(packet.trace), packet.latency, port, 0]
         # else:               
             # self.DV[packet.dst] = [len(packet.trace), packet.latency, port, 0]
-        self.DV[port][packet.dst] = (packet.latency, 0)
-        self.chek_for_better_path(packet.dst)
+        if not port in self.DV:
+            self.DV[port] = {}
+        self.DV[port][packet.destination] = (packet.latency, 0)
+        self.chek_for_better_path(packet.destination)
     elif isinstance(packet, basics.HostDiscoveryPacket):
         #self.vectors[packet.src] = [1, self.neighbors[port], port, -1]
         self.me[packet.src] = (self.neighbors[port], port,-1) # set like {... h1:(1,8) ...} for own DV
@@ -112,42 +107,47 @@ class DVRouter (basics.DVRouterBase):
     for port,vector in self.DV.iteritems():
         obsolete = []
         for k,v in vector.iteritems():
-            if v[1] >= 15:
+            l,t = v
+            if t >= 15:
                 obsolete.append(k)
             else:
-                v[1] += 5 # need to chek if it's correct
+                self.DV[port][k] = (l, t + 5)
         for i in obsolete:
             del vector[i] 
-            
-    
 
     #send update
     obsolete = []
     for dest in self.me.keys():
         l,p,t = self.me[dest]
-        if t<15:
+        if t!= -1:
+            t += 5
+            self.me[dest] = (l,p,t)
+        #pdb.set_trace()
+        if t<15 and l!=INFINITY:
             self.send_all_update(dest)
         else:
             obsolete.append(dest)
             
     for i in obsolete:
         del self.me[i] 
-     
-    
 
   """
     Sends one route update to all neighbor routers (no hosts)
   """
   def send_one_update(self, dest, port):  
       latency,p,t = self.me[dest]
+      # if t == -1:
+        # return
       packet = basics.RoutePacket(dest, latency)
+      #pdb.set_trace()
       if port!=p:                                   # split horizon      
         self.send(packet, port, flood=False)
-      elif POISON_MODE and port == p:               # poison reverse
+      elif self.POISON_MODE and port == p:               # poison reverse
         packet = basics.RoutePacket(dest, INFINITY)
         self.send(packet, port, flood=False)
       
-  def chek_for_better_path(dest):
+  def chek_for_better_path(self, dest):
+      #pdb.set_trace()
       for k,v in self.DV.iteritems():
         latency,p,t = self.me[dest]
         l,ttl = v[dest]
@@ -155,8 +155,7 @@ class DVRouter (basics.DVRouterBase):
             self.me[dest] = (l + neighbors[k],k, ttl)
             
   def send_all_update(self, dest):
-      for port in self.me.keys():
-        latency,p,t = self.me[dest]
+      for port in self.neighbors.keys():
         self.send_one_update(dest, port)
 
 
