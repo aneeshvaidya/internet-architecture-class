4c7a70d0a13cd9ae026227c38688d34db05d1a80
rahim77 <krutchm@gmail.com>
2015-10-05 20:29:21 -0700

    test loop

--- project1/simulator/examples/test_loop.py
+++ project1/simulator/examples/test_loop.py
@@ -3,9 +3,11 @@ Test routing with a link failure
 
 Creates a topology like:
 
-h1 -- s1 ---- s2 -- h2
-        \   /
-         s3 
+            s5
+        /   |    \
+h1 -- s1 -- s2 -- s4 -- h2
+        \   |    /
+            s3 
 
 Sends a ping from h1 to h2.
 Waits a while.
@@ -31,18 +33,21 @@ def launch ():
   s1 = sim.config.default_switch_type.create('s1')
   s2 = sim.config.default_switch_type.create('s2')
   s3 = sim.config.default_switch_type.create('s3')
-  # s4 = sim.config.default_switch_type.create('s4')
-  # s5 = sim.config.default_switch_type.create('s5')
+  s4 = sim.config.default_switch_type.create('s4')
+  s5 = sim.config.default_switch_type.create('s5')
 
   h1.linkTo(s1)
-  h2.linkTo(s2)
+  h2.linkTo(s4)
 
-  s1.linkTo(s2, latency =3)
+  s1.linkTo(s2, latency =2)
 
   s1.linkTo(s3)
+  s1.linkTo(s5)
+  s4.linkTo(s3)
+  s4.linkTo(s2)
+  s4.linkTo(s5)
+  s5.linkTo(s2)
   s3.linkTo(s2)
-  # s4.linkTo(s5)
-  # s5.linkTo(s2)
 
   def test_tasklet ():
     yield 5 # Wait five seconds for routing to converge
@@ -52,10 +57,10 @@ def launch ():
 
     yield 5
 
-    api.userlog.debug("Failing s1-s2 link ")
-    pdb.set_trace()
+    api.userlog.debug("Adding fast s1-s2 link ")
+    #pdb.set_trace()
     #s1.unlinkTo(s2)
-
+    s1.linkTo(s2, latency=2)
     yield 10
 
     api.userlog.debug("Sending test ping 2")
@@ -63,7 +68,7 @@ def launch ():
     #pdb.set_trace()
     yield 10
 
-    s1.linkTo(s2, latency=2)
+
 
     if h2.pings != 2:
       api.userlog.error("h2 got %s packets instead of 2", h2.pings)
